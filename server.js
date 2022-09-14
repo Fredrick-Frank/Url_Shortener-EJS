3b987321f4d160fe18e7c6b776372feed4696ea0
Fredrick-Frank <igwefredrickchiemeka@gmail.com>
2022-09-14 16:27:11 +0100

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 //connecting to our database
 //to use the env file:
